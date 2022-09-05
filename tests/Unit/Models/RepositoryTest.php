b1e2f0d73bb852d7dd0fc9723dbbcce51127147c
Laura F. García <52688109+LauraFgar@users.noreply.github.com>
2022-09-04 21:14:16 -0500

    inicio del proyecto

---
+++ tests/Unit/Models/RepositoryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Repository;
+use App\Models\User;
+use Tests\TestCase;
+
+class RepositoryTest extends TestCase
+{
+    
+    public function test_belongs_to_user()
+    {
+        $repository = Repository::factory()->create();
+        $this->assertInstanceOf(User::class, $repository->user);
+    }
+}
